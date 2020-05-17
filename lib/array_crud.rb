d6967e6217ceb64ea2d7fbe2d2736e3155f7ea50
eager-class-5814 <candacehchoe@gmail.com>
2020-05-17 23:27:55 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,7 @@ end
 
 def add_element_to_end_of_array(array, element)
 create_an_array << "nala cat"
-p add_element_to_end_of_array
+p create_an_array
 end
 
 def add_element_to_start_of_array(array, element)
